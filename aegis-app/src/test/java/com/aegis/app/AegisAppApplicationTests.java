395fd8d919c31c435356722507bd5a17f2602b93
pzy <puzhiyuan2005@icloud.com>
2026-04-05 13:13:51 +0800

    initial

---
+++ aegis-app/src/test/java/com/aegis/app/AegisAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aegis.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AegisAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
